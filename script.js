c35f540a5b423400cee98945e825a36fa08bca75
Ogunyemi Richard Ayooluwa <143848799+Lordricho@users.noreply.github.com>
2026-07-18 11:19:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hide Loading Screen
 window.onload = function () {
     const loader = document.getElementById("loader");
 
@@ -5,3 +6,20 @@ window.onload = function () {
         loader.style.display = "none";
     }
 };
+
+// Mobile Menu
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", function () {
+        navLinks.classList.toggle("show-menu");
+    });
+}
+
+// Close menu after clicking a link
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("show-menu");
+    });
+});
